e374a255448377ac92b6f8995d1f089770f8f841
Peter-SQL <pblechert@gmail.com>
2026-08-27 20:38:52 +0200

    model

--- sql/ddl/star_schema.sql
+++ sql/ddl/star_schema.sql
@@ -42,16 +42,16 @@ CREATE TABLE analytics.dim_airport_id (
 );
 
 
--- analytics.dim_cancel_reason definition
+-- analytics.dim_cancellationcode definition
 
 -- Drop table
 
--- DROP TABLE analytics.dim_cancel_reason;
+-- DROP TABLE analytics.dim_cancellationcode;
 
-CREATE TABLE analytics.dim_cancel_reason (
-	cancel_code varchar(1) NOT NULL,
-	cancel_reason varchar(50) NOT NULL,
-	CONSTRAINT dim_cancel_reason_pkey PRIMARY KEY (cancel_code)
+CREATE TABLE analytics.dim_cancellationcode (
+	cancellationcode varchar(1) NOT NULL,
+	cancellationreason varchar(50) NOT NULL,
+	CONSTRAINT dim_cancel_reason_pkey PRIMARY KEY (cancellationcode)
 );
 
 
@@ -81,24 +81,28 @@ CREATE TABLE analytics.dim_date (
 CREATE TABLE analytics.fact_flight (
 	flight_id int8 GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE) NOT NULL,
 	flightdate date NOT NULL,
-	dot_id_reporting_airline int4 NULL,
-	flight_number_reporting_airline int4 NULL,
-	originairportid int4 NULL,
-	destairportid int4 NULL,
-	departuredelaygroups int4 NOT NULL,
-	deptimeblk varchar(10) NOT NULL,
-	taxiin int4 NOT NULL,
-	taxiout int4 NOT NULL,
-	arrivaldelaygroups int4 NOT NULL,
+	dot_id_reporting_airline int4 NOT NULL,
+	flight_number_reporting_airline int4 NOT NULL,
+	originairportid int4 NOT NULL,
+	origin varchar(3) NULL,
+	destairportid int4 NOT NULL,
+	dest varchar(3) NULL,
+	departuredelaygroups int4 NULL,
+	deptimeblk varchar(10) NULL,
+	taxiin int4 NULL,
+	taxiout int4 NULL,
+	arrivaldelaygroups int4 NULL,
 	cancelled bool NOT NULL,
+	cancellationcode varchar(1) NULL,
 	diverted bool NOT NULL,
-	distance int4 NOT NULL,
-	carrierdelay int4 NOT NULL,
-	weatherdelay int4 NOT NULL,
-	nasdelay int4 NOT NULL,
-	securitydelay int4 NOT NULL,
-	lateaircraftdelay int4 NOT NULL,
+	distance int4 NULL,
+	carrierdelay int4 NULL,
+	weatherdelay int4 NULL,
+	nasdelay int4 NULL,
+	securitydelay int4 NULL,
+	lateaircraftdelay int4 NULL,
 	CONSTRAINT fact_flight_pkey PRIMARY KEY (flight_id),
+	CONSTRAINT fact_flight_cancellationcode_fkey FOREIGN KEY (cancellationcode) REFERENCES analytics.dim_cancellationcode(cancellationcode),
 	CONSTRAINT fact_flight_destairportid_fkey FOREIGN KEY (destairportid) REFERENCES analytics.dim_airport_id(airport_id),
 	CONSTRAINT fact_flight_dot_id_reporting_airline_fkey FOREIGN KEY (dot_id_reporting_airline) REFERENCES analytics.dim_airline(dot_id_reporting_airline),
 	CONSTRAINT fact_flight_flightdate_fkey FOREIGN KEY (flightdate) REFERENCES analytics.dim_date(full_date),
